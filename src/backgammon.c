f85620c7cd6faa8c3be3e05a7bcd0df80ec8f203
KenTiN <quentin.lebourgeois@gmail.com>
2011-12-20 14:45:42 +0100

    fusion des mains

--- src/backgammon.c
+++ src/backgammon.c
@@ -3,31 +3,71 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <dlfcn.h>
+// Includes SDL
+#include <SDL/SDL.h>
+#include <SDL/SDL_rotozoom.h>
 // Includes persos
 #include "../include/engine.h"
+#include "../include/display.h"
 #include "../lib/game_ai.h"
 
 int main(int argc, char *argv[])
 {
 	void *lib;
-	typedef void (*fptr_AI_start_match)(int);
-	fptr_AI_start_match AI_start_match;
-
-	if ((lib = dlopen("libgame_ai.so", RTLD_LAZY)) == NULL)
-	{
-		printf("Erreur d'ouverture : %s\n", dlerror());
-		return 1;
+	
+	// Vérification des args de la ligne de commande
+	if (argc < 2) {
+		fprintf(stderr, "usage: %s <lib_path>\n", *(argv));
+		exit(1);
 	}
-	if ((AI_start_match=(fptr_AI_start_match)dlsym(lib, "start_match")) == NULL)
+	
+	// Ouverture dynamique de la bibliothèque
+	if ((lib = dlopen(*(argv + 1), RTLD_LAZY)) == NULL)
 	{
-		printf("Erreur de lecture");
-		return 1;
+		fprintf(stderr, "Error: %s\n", dlerror());
+		exit(1);
 	}
+	
+	display_manager d_manager;
+	
+    init_display(&d_manager, "./img/defaut/");
+    int continuer = 1;
+    SDL_Event event;
 
-	AI_start_match(10);
+    while (continuer)
+    {
+    	interface_display(&d_manager);
+		
+        SDL_PollEvent(&event);
+        Uint8 *keystates = SDL_GetKeyState( NULL );
+		
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+            case SDL_KEYDOWN:
+            	
+            	if( keystates[ SDLK_LEFT ] ) { 
+            		switch_to_full_screen(&d_manager);
+				}
+				if( keystates[ SDLK_RIGHT ] ) { 
+					 switch_to_window(&d_manager);
+				}
+				if( keystates[ SDLK_UP ] ) { 
+					continuer = 0;
+				}
 
-	dlclose(lib);
+            	
+        }
 
-	return 0;
+        SDL_Flip(d_manager.screen);
+    }
+
+    free_surface(&d_manager);
+
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
+
 
