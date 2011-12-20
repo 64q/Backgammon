8a0d2fd48f884e8aa5cbc4da781ffdd072c82461
KenTiN <quentin.lebourgeois@gmail.com>
2011-12-20 16:36:13 +0100

    restauration lib/

---
+++ lib/ai.c
@@ -0,0 +1,31 @@
+// Includes standards
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+// Includes persos
+#include "../include/backgammon.h"
+#include "ai.h"
+
+void InitLibrary(char name[50]){}
+
+void StartMatch(const unsigned int target_score){}
+
+void StartGame(){}
+
+void EndGame(){}
+
+void EndMatch(){}
+
+
+int DoubleStack(const SGameState * const gameState)
+{
+	return 0;
+}
+
+int TakeDouble(const SGameState * const gameState)
+{
+	return 0;
+}
+
+void MakeDecision(const SGameState * const gameState, SMove moves[4], unsigned int lastTimeError){}
+
